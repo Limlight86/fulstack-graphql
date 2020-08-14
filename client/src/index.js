9865288e52b4e95b188b35f9e741689afe1c5c88
Lazaro <lquin008@gmail.com>
2020-08-13 21:55:39 -0400

    apollo provider set up

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { ApolloProvider } from '@apollo/react-hooks'
 import App from './components/App'
 import './index.css'
+import client from "./client"
 
 const Root = () => (
   <BrowserRouter>
