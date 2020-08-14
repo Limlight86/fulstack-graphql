c0ff952a0be1ae05a9201c9a08647d0653138350
Lazaro <lquin008@gmail.com>
2020-08-13 21:23:57 -0400

    apollo link/cache/client set up

--- client/src/client.js
+++ client/src/client.js
@@ -6,3 +6,13 @@ import gql from 'graphql-tag'
 /**
  * Create a new apollo client and export as default
  */
+
+ const link = new HttpLink({uri: "http://localhost:4000/"});
+ const cache = new InMemoryCache();
+
+ const client = new ApolloClient({
+   link,
+   cache
+ });
+
+ export default client
