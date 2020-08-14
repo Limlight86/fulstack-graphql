a67ab756f9b20cbbe69dbb81149c3ee273a70a45
Lazaro <lquin008@gmail.com>
2020-08-13 22:16:59 -0400

    all pets query set up on pet page

--- client/src/pages/Pets.js
+++ client/src/pages/Pets.js
@@ -5,13 +5,30 @@ import PetsList from '../components/PetsList'
 import NewPetModal from '../components/NewPetModal'
 import Loader from '../components/Loader'
 
-
+const ALL_PETS = gql`
+  query AllPets{
+    pets{
+      id
+      type
+      name
+      img
+    }
+  }
+`
 export default function Pets () {
   const [modal, setModal] = useState(false)
-
+  const {data, loading, error} = useQuery(ALL_PETS)
 
   const onSubmit = input => {
     setModal(false)
+  }
+
+  if (loading){
+    return <Loader/>
+  }
+
+  if (error){
+    console.log(error)
   }
   
   if (modal) {
@@ -32,7 +49,7 @@ export default function Pets () {
         </div>
       </section>
       <section>
-        <PetsList />
+        <PetsList pets={data.pets}/>
       </section>
     </div>
   )
